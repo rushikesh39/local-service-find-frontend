15f0472f775c0ca2ba71b5425b8c782073bbbce1
rushi <rushikeshthange2001@gmail.com>
2025-06-23 13:18:20 +0530

    update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,6 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
-import Booking from "../Pages/Booking";
 import bookingReducer from "./bookingSlice";
 import userReducer from '../redux/userSlice'
 import servicesReducer from '../redux/servicesSlice'
